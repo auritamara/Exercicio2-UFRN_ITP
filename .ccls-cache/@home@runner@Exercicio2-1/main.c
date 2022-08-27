ed7afd31d13ed13b7de0fed318cd8fbd4f220c52
Tâmara Gomes <112263932+auritamara@users.noreply.github.com>
2022-08-27 18:38:45 +0000

    Resolver exercícios

--- .ccls-cache/@home@runner@Exercicio2-1/main.c
+++ .ccls-cache/@home@runner@Exercicio2-1/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  int valorDigitado;
+  scanf ("%i", &valorDigitado);
+  int anos = 0;
+  int meses = 0;
+  int dias = 0;
+  if (valorDigitado > 364) {    
+    for (int i = 0; valorDigitado > 364; i++){
+      anos++;
+      valorDigitado = valorDigitado - 365;      
+    }
+  }
+  if (valorDigitado > 29) {
+    for (int i = 0; valorDigitado > 29; i++){
+      meses++;
+      valorDigitado = valorDigitado - 30;      
+    }
+  }
+  dias = valorDigitado;
+  printf("%i ano(s)\n", anos);
+  printf("%i mes(es)\n", meses);
+  printf("%i dia(s)\n", dias);
   return 0;
 }
